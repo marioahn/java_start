1a86546012dd13aff7ca8579127234ad53766767
marioahn <marioahn@naver.com>
2024-03-24 11:05:58 +0900

    basic1

---
+++ src/HelloJava1.java
@@ -0,0 +1,11 @@
+public class HelloJava1 {
+    // psvm하면 아래 나옴
+    public static void main(String[] args) {
+        // sout하면 print문
+        System.out.println("hi1");
+        System.out.println("'h2'"); // java는 따옴표는 안먹히네
+        System.out.println("hi3");
+    }
+}
+
+
---
+++ src/variable1/Var1.java
@@ -0,0 +1,11 @@
+package variable1;
+// 폴더가 패키지임. 근데 폴더개념은 아닌것 같긴 함, 쨋든 intellij에서 자동 코드생성
+// 여기 코드 이름과 폴더이름 다르면 x
+
+public class Var1 {
+    public static void main(String[] args) {
+        System.out.println(20);
+        System.out.println(10);
+        System.out.println(10);
+    }
+}
